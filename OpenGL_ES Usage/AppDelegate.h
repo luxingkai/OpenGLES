1e442c3602df04de596aba8378177cc58c5c77ac
luxingkai <18356285335@163.com>
2020-03-20 11:04:40 +0800

    Initial Commit

---
+++ OpenGL_ES Usage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES Usage
+//
+//  Created by tiger fly on 2020/3/20.
+//  Copyright © 2020 tiger fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGL_ES Usage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL_ES Usage
+//
+//  Created by tiger fly on 2020/3/20.
+//  Copyright © 2020 tiger fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
